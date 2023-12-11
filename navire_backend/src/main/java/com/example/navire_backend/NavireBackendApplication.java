8be6caf57c3af13c88bb3f20353e9232244995d9
amir <amirzouari23@gmail.com>
2023-12-11 19:53:48 +0100

    empty folders

---
+++ navire_backend/src/main/java/com/example/navire_backend/NavireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.navire_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavireBackendApplication.class, args);
+	}
+
+}
